81472b947bf9f785a265a1640a2e0ff488fda5a9
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-06-07 23:10:11 +0530

    Add files via upload

---
+++ Shortstrings.java
@@ -0,0 +1,11 @@
+package Module_7;
+
+public class Shortstrings {
+    public static void main(String[] args) {
+        String str[]={"kishan","hi","all","ball","cat","bat"};
+        java.util.Arrays.sort(str);
+        for (String s : str) {
+            System.out.print(s+" ");
+        }
+    }
+}
