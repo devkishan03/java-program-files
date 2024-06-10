e6b35a6fd5c30c1ea30ecc71facdb9322b3dcf35
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-06-10 23:03:22 +0530

    Add files via upload

---
+++ SumofallElement.java
@@ -0,0 +1,14 @@
+package Module_7.Arrays;
+
+public class SumofallElement {
+    public static void main(String[] args) {
+        int A[]={1,2,3,10,11,23,45,78};
+          int sum=0;
+        for(int i=0;i<A.length;i++){
+               sum=sum+A[i];
+        }
+         System.out.println(sum);
+
+
+    }
+}
