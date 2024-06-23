3e66770111ee8d7c62aabc92e5d82bc46c60e6ed
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-06-23 23:57:11 +0530

    Add files via upload

--- Validate.java
+++ Validate.java
@@ -1,16 +1,14 @@
+package Module_8;
 
-
-public class Validate 
-{
+public class Validate {
     static boolean validate(String name){
         return name.matches("[a-zA-Z\\s]+");
     }
-    static boolean validate(int a){
-        return a>=3&&a<=15;
+    static boolean validate(int age){
+       return 3<age && age<14 ? true:false;
     }
     public static void main(String[] args) {
         System.out.println(validate("kishan singh"));
-        System.out.println(validate(14));
-
+        System.out.println(validate(15));
     }
 }
