37817167f50605fa0f4e64ae3af2cc4a5c714c55
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-07-05 23:07:52 +0530

    Add files via upload

--- Example.java
+++ Example.java
@@ -1,61 +1,43 @@
-package Module_9_Oops.Constructor_inheritence;
-class Rectangle
+package Module_9_Oops.Dynamic_method_dispatch;
+class Super
 {
-    int length;
-    int braith;
-    int x=10;
-
-    public Rectangle(){
-        length=braith=1;
+    public void method1(){
+        System.out.println("Super class meth1");
     }
-    public Rectangle(int l,int b){
-        length=l;
-        braith=b;
-    }
-
-    public int area(){
-        return length*braith;
-    }
-    public int perimeter(){
-          return 2*length+braith;
-    }
-
-}
-
-
-class Couboid extends Rectangle
-{
-    int height;
-    int x=20;
-
-    public Couboid(){
-        height=1;
-    }
-    public Couboid(int h){
-          height=h;
-    }
-    public Couboid(int l,int b,int h){
-         super(l,b);
-          height=h;
-    }
-
-    public int volume(){
-       return area()*height;
+    public void method2(){
+        System.out.println("Super class meth2");
     }
 }
 
-
-
+class Sub extends Super
+{
+    @Override
+    public void method2(){
+        System.out.println("Sub class meth2");
+    }
+    public void method3(){
+         System.out.println("Sub class meth3");
+    }
+}
 public class Example {
-    public static void main(String[] args){
-        Rectangle r=new Rectangle();
-        Couboid c=new Couboid(10,8,5);
-        
-        System.out.println(c.area());
-        System.out.println(c.volume());
-        System.out.println(c.x);
-        System.out.println(r.x);
+    public static void main(String[] args) {
+        Super s=new Super();
+        s.method1();
+        s.method2();
 
+       System.out.println();
+
+        Sub s2=new Sub();
+
+        s2.method1();
+        s2.method2();
+        s2.method3();
+
+        Super s3=new Sub();
+
+        s3.method1();
+        s3.method2();      //dynamic method dispatch
+       
 
     }
 }
