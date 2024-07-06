7120a574af6e9a39376d6ef6970cc3c26d1f86c9
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-07-06 23:00:34 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package Module_9_Oops.Final;
+
+final class Super
+{
+   final int a=10;
+
+   final void meth(){
+      System.out.println("hello");
+   }
+
+}
+public class Demo {
+    public static void main(String[] args) {
+        
+    }
+}
