62d7eb67870179e0a27c23e1530a5856864f2ff2
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-09-24 00:03:01 +0530

    Add files via upload

--- Storeabove50.java
+++ Storeabove50.java
@@ -10,11 +10,27 @@ public class Storeabove50 {
         return newarr;
     }
 
-    public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 51, 50, 51, 52, 52, 60, 60, 70, 80, 90, 90, 87, 78, 65, 56, 65, 55 };
-        int arr2[] = storeabovefifty(arr);
-        for (int i : arr2) {
-            System.out.print(i);
+    public static int findsecondlargest(int arr[]) {
+        int max = arr[0];
+        int smax = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                smax = max;
+                max = arr[i];
+            } else if (arr[i] > smax && arr[i] < max) {
+                smax = arr[i];
+            }
         }
+        return smax;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 51, 50, 51, 52, 52, 60, 60, 70, 88, 80, 90, 90, 87, 78, 65, 56, 65, 55 };
+        int arr2[] = storeabovefifty(arr);
+        // for (int i : arr2) {
+        // System.out.print(i);
+        // }
+
+        System.out.println(findsecondlargest(arr));
     }
 }
